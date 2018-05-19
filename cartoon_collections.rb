c27c4c64e0a523608a2ee0cbfcaff0453dc624e9
Natalia Castro <nat.alvz@gmail.com>
2018-05-19 03:32:30 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -17,7 +17,7 @@ def long_planeteer_calls(array)
 end
 
 def find_the_cheese(array)
-  
+  array.include? 
 def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
